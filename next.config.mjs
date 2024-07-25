51c964adcd3b5b9d3276e2f642e8c7dc879382e3
Marko Vračaričić <marko.vracaricic.83@gmail.com>
2024-07-25 20:11:11 +0200

    config.next.mjs changed

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode:true,
   swcMinify:true,
     experimental: {
-      missingSuspenseWithCSRBailout: false,
+    //  missingSuspenseWithCSRBailout: false,
      // appDir: true,
       serverComponentsExternalPackages: ["mongoose"],
     },
