93868fb922245543ecd9dc091e4b2f0ea97f081e
Marko Vračaričić <marko.vracaricic.83@gmail.com>
2024-07-27 21:21:51 +0200

    Small css corection on title and dropdown

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,8 @@ import Feed from "@components/Feed"
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-        <h1 className="head_text text-center" id="title">
-            Tripoteka <br className="max-md:hidden" />
+        <h1 id="title" className="head_text text-center" >
+            Tripoteka <br  />
             <span className="orange_gradient text-center">Teme o kojima se priča</span>
         </h1>
         <p className="desc text-center">
