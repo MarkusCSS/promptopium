d9c4f2c436648f260bdac00dcce2bbde9053f269
Marko Vračaričić <marko.vracaricic.83@gmail.com>
2024-07-27 20:25:14 +0200

    Text change at main page-title

--- app/page.jsx
+++ app/page.jsx
@@ -4,13 +4,12 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
-            (Ne)Kolegijalnost  <br className="max-md:hidden" />
+            Tripoteka <br className="max-md:hidden" />
             <span className="orange_gradient text-center">Teme o kojima se priča</span>
         </h1>
         <p className="desc text-center">
            Mesto gde se mogu postaviti pitanja i dobiti 
-           odgovori o svemu što vas tišti, kako reagovati,
-           sutprotsaviti se svakom obliku ugrožavanja na radnom mestu
+           odgovori.Odgovori koje želite da čujete.Vizija bez akcije je samo halucinacija.
         </p>
       <Feed/>
     </section>
