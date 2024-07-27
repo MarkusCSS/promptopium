a5ceb4bde6a046e636e5a22e1a6d2e2534c1033d
Marko Vračaričić <marko.vracaricic.83@gmail.com>
2024-07-27 20:46:02 +0200

    Animation added on #title repeated

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Feed from "@components/Feed"
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-        <h1 className="head_text text-center">
+        <h1 className="head_text text-center" id="title">
             Tripoteka <br className="max-md:hidden" />
             <span className="orange_gradient text-center">Teme o kojima se priča</span>
         </h1>
