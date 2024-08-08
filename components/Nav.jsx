a29c31eb7b486b399100a3642a944acf2506c237
Marko Vračaričić <marko.vracaricic.83@gmail.com>
2024-08-08 23:59:53 +0200

    Resize logo responsivity

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -27,7 +27,7 @@ const Nav = () => {
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 rounded-md items-center justify-center'>
-  <div className='flex items-center justify-center relative  w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 2xl:w-16 2xl:h-16'>
+  <div className='flex items-center justify-center relative  w-12 h-12 md:w-14 md:h-14 lg:w-16 lg:h-16 2xl:w-18 2xl:h-18'>
     <Image
       src='/assets/images/logo.png'
       alt='logo'
