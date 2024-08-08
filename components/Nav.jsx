42b7261278313302ed809efc74eb45e6b6489ad8
Marko Vračaričić <marko.vracaricic.83@gmail.com>
2024-08-08 05:09:21 +0200

    logo changed

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -28,11 +28,11 @@ const Nav = () => {
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
         <Image
-          src='/assets/images/logo.svg'
+          src='/assets/images/logo.png'
           alt='logo'
-          width={30}
-          height={30}
-          className='object-contain'
+          width={60}
+          height={60}
+          className='object-contain rounded'
         />
         <p className='logo_text'>Tripoteka</p>
       </Link>
